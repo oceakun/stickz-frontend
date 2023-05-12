4058b9f9accc734bd632639d8d7709426b8747ed
Sagar Deep <oceakun@pop-os.localdomain>
2023-05-12 17:52:42 +0530

    folder renamed to-utilities

---
+++ src/components/utilies/Draw.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import Sidebar from "../sidebar/Sidebar";
+interface Props {}
+
+const Draw = () => {
+  return (
+    <DrawContainer>
+      <Sidebar />
+      draw
+    </DrawContainer>
+  );
+};
+
+export default Draw;
+
+const DrawContainer = styled.div`
+  color: var(--text);
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 20px;
+  margin: 0 10px 0 0;
+`;
---
+++ src/components/utilies/Lists.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import styled from "styled-components";
+import Sidebar from "../sidebar/Sidebar";
+
+interface Props {}
+
+const Lists = () => {
+  return (
+    <ListsContainer>
+      <Sidebar />
+      lists
+    </ListsContainer>
+  );
+};
+
+export default Lists;
+
+const ListsContainer = styled.div`
+  color: var(--text);
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-start;
+  justify-content: space-between;
+  gap: 20px;
+  margin: 0 10px 0 0;
+`;
